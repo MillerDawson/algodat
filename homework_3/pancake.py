956cbd9c39749f0006e56891ec542d63581a26cb
Miller Dawson <63493106+MillerDawson@users.noreply.github.com>
2024-03-02 18:46:11 +0000

    Update pancake.py
    skeleton code added to pancake.py

---
+++ homework_3/pancake.py
@@ -0,0 +1,15 @@
+import sys
+
+def pancake_sort(list: list):
+    """ function which takes in stack as a list from bottom to top IE, 5, 4, 3, 2, 1 """
+    print(line_array)
+    pass
+
+# input section
+line = sys.stdin.readline().strip()
+while line != "0":
+    line_array = list(map(int, line.split()))
+    print(pancake_sort(line_array))
+    line = sys.stdin.readline().strip()
+    
+print("0")
