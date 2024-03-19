10e924ed3b9562a8c6c2cbb506641cfd3606484e
Miller Dawson <63493106+MillerDawson@users.noreply.github.com>
2024-03-19 03:09:15 +0000

    Update mission.py
    some progress on mission.py - was meant to pushed yesterday but forgot

--- homework_4/mission.py
+++ homework_4/mission.py
@@ -1,18 +1,31 @@
 import sys
 
-def can_drive(num_villages,roads):
-  """You should implement this function, which takes in the number
-     of villages in the problem and a set of tuples representing
-     the start end end village of each road. 
-     
-     It returns 1 if one can drive, or -1 if one cannot drive as per the instructions."""
+def is_connected(graph, vertex, visited):
   pass
+
+def can_drive(num_villages,roads):
+  # initialise visited
+  visited = [False] * num_villages
+  
+  # creates empty sets for each village
+  graph = {i: set() for i in range(num_villages)}
+  
+  
+  for (start_road, end_road) in roads:
+    graph[start_road].add(end_road)
+    graph[end_road].add(start_road)
+  
+  # check if all vertices have been visited
+  
+  # check if graph is eulerian cycle
+  for ver in graph.values():
+    if len(ver) &2 != 0: # ensures each vertex of graph has even number degrees
+      return -1 
+
   
     
-    
-
+# ===== User Section =====
 num_problems=int(sys.stdin.readline().strip())
-output=""
 for _ in range(num_problems):
   line=sys.stdin.readline().strip()
   num_villages,num_roads=list(map(int,line.split()))
