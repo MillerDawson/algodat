7a7798eb1adcdfab5cf385167ac7b1eb8e0874b8
Miller Dawson <63493106+MillerDawson@users.noreply.github.com>
2024-03-19 04:36:10 +0000

    Update mission.py
    basic graph logic, implemented, however tests fail. Must debug code

--- homework_4/mission.py
+++ homework_4/mission.py
@@ -1,27 +1,44 @@
 import sys
 
-def is_connected(graph, vertex, visited):
-  pass
+# https://www.geeksforgeeks.org/eulerian-path-and-circuit/ - Reference Used Throughout
+
+# ===== Logic Section =====
+def is_connected(graph, curr, visited):
+  visited[curr] = True # sets current vertex to visited
+  
+  # recursively for all vertex adjacent to this to check if eulerian cycle
+  for vertex in graph[curr]:
+    if not visited[vertex]:
+      is_connected(graph, vertex, visited)
 
 def can_drive(num_villages,roads):
-  # initialise visited
-  visited = [False] * num_villages
+  visited = [False] * num_villages # initialise visited
+  graph = {i: set() for i in range(num_villages)} # initialise graph
   
-  # creates empty sets for each village
-  graph = {i: set() for i in range(num_villages)}
-  
-  
+  # sets graph with appropriate start/end roads
   for (start_road, end_road) in roads:
     graph[start_road].add(end_road)
     graph[end_road].add(start_road)
   
   # check if all vertices have been visited
+  find_zero_vertiex = next((village for village, val in enumerate(graph.values()) if val), False)
+  
+  if find_zero_vertiex is False:
+    return 1 # graph is a eulerian cycle
+  
+  is_connected(graph, find_zero_vertiex, visited)
+  
+  # checks if graph is connected
+  for i in range(num_villages):
+    if graph[i] and not visited[i]:
+      return -1 
   
   # check if graph is eulerian cycle
-  for ver in graph.values():
-    if len(ver) &2 != 0: # ensures each vertex of graph has even number degrees
+  for vertex in graph.values():
+    if len(vertex) &2 != 0: # ensures each vertex of graph has even number degrees
       return -1 
-
+    
+  return 1 # graph meets all requirements is traversible with eulerian cycle present
   
     
 # ===== User Section =====
